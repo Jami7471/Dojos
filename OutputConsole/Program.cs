d5b09d0e3213735cfb015cf02e1fc4bbe7d116f3
Jens Gross <j_gross@live.de>
2022-12-23 13:15:42 +0100

    Task 10 of 2022

--- OutputConsole/Program.cs
+++ OutputConsole/Program.cs
@@ -14,7 +14,7 @@ timeAnalyse.AllLoopOfMethodFinished += TimeAnalyse_AllLoopOfMethodFinished;
 
 
 // _ = timeAnalyse.DoAllPublicMethodsAsync("2015.10", typeof(AdventOfCode.Year2015.Task10), 100);
-timeAnalyse.DoAllPublicMethods("2022.09", typeof(AdventOfCode.Year2022.Task09));
+timeAnalyse.DoAllPublicMethods("2022.10", typeof(AdventOfCode.Year2022.Task10));
 
 
 
